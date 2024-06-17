fd6201cfc84b17842e9d0525b323420c97a53beb
Shilok.k (Student) <138785910+web4389@users.noreply.github.com>
2024-06-17 23:46:54 +0500

    Update loginSlice.js

--- Frontend/src/features/loginSlice.js
+++ Frontend/src/features/loginSlice.js
@@ -7,6 +7,7 @@ export const login = createAsyncThunk(
       method: "POST",
       headers: {
         "Content-Type": "application/json",
+        "Access-Control-Allow-Origin": "*"
       },
       body: JSON.stringify({ email, password }),
     });
